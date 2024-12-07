0142e5f44e2fa0fb6d43bee647212823894b92f5
ArturMarekNowak <arturmareknowal@gmail.com>
2024-12-07 21:41:38 +0100

    Add AoC 2024 day 7

---
+++ 2024/7.1/app.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	equationsFile := OpenFile("/2024/7.1/input1.1.txt")
+	equations := ExtractLines(equationsFile)
+	correctResults := CalculateCorrectEquations(equations)
+	sum := Sum(correctResults)
+
+	fmt.Println(sum)
+}
+
+func Sum(slice []int) int {
+	cnt := 0
+	for _, i := range slice {
+		cnt += i
+	}
+	return cnt
+}
+
+func CalculateCorrectEquations(equations []Equation) []int {
+	var results []int
+	for i := 0; i < len(equations); i++ {
+		if IsEquationPossible(equations[i]) {
+			results = append(results, equations[i].result)
+		}
+	}
+
+	return results
+}
+
+func IsEquationPossible(equation Equation) bool {
+	possibleSignsCombinations := CalculatePossibleSignsCombinations(len(equation.products) - 1)
+
+	fmt.Println(possibleSignsCombinations)
+
+	for i := 0; i < len(possibleSignsCombinations); i++ {
+		result := equation.products[0]
+		for j := 0; j < len(possibleSignsCombinations[i]); j++ {
+
+			if possibleSignsCombinations[i][j] == '+' {
+				result += equation.products[j+1]
+			} else if possibleSignsCombinations[i][j] == '*' {
+				result *= equation.products[j+1]
+			} else {
+				panic("Nope")
+			}
+		}
+
+		if equation.result == result {
+			return true
+		}
+	}
+
+	return false
+}
+
+func CalculatePossibleSignsCombinations(k int) []string {
+	signs := []string{"+", "*"}
+	result := []string{}
+
+	CalculatePossibleSignsCombinationsRecursively(signs, "", len(signs), k, &result)
+
+	return result
+}
+
+func CalculatePossibleSignsCombinationsRecursively(signs []string, prefix string, n int, k int, result *[]string) {
+	if k == 0 {
+		*result = append(*result, prefix)
+		return
+	}
+
+	for j := 0; j < n; j++ {
+		newPrefix := prefix + signs[j]
+		CalculatePossibleSignsCombinationsRecursively(signs, newPrefix, n, k-1, result)
+	}
+}
+
+type Equation struct {
+	result   int
+	products []int
+}
+
+func ExtractLines(file *os.File) []Equation {
+	scanner := bufio.NewScanner(file)
+	var result []Equation
+	for scanner.Scan() {
+		sanitizedLines := strings.Replace(scanner.Text(), ":", "", -1)
+		numbersAsStrings := strings.Split(sanitizedLines, " ")
+		numbersAsInts := ConvertStringsArrayToIntsArray(numbersAsStrings)
+
+		result = append(result, Equation{result: numbersAsInts[0], products: numbersAsInts[1:]})
+	}
+
+	return result
+}
+
+func ConvertStringsArrayToIntsArray(stringsArray []string) []int {
+	var result []int
+	for _, str := range stringsArray {
+		number, _ := strconv.Atoi(str)
+		result = append(result, number)
+	}
+
+	return result
+}
+
+func OpenFile(filename string) *os.File {
+	pwd, _ := os.Getwd()
+	file, err := os.Open(pwd + filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return file
+}
